304769dfb96b2a45c185ee94640e94336228b609
unknown <marcin7848@gmail.com>
2021-02-15 11:24:38 +0100

    widget start

---
+++ app/src/main/java/com/dailyroutinetasks/DailyRoutineTasksWidget.java
@@ -0,0 +1,43 @@
+package com.dailyroutinetasks;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+
+public class DailyRoutineTasksWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.daily_routine_tasks_widget);
+
+        remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
