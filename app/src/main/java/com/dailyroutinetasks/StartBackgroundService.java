3bf3eab0e9847afb818bc972aa97ea22f97d8bed
unknown <marcin7848@gmail.com>
2021-02-15 13:20:07 +0100

    background and foreground services

--- app/src/main/java/com/dailyroutinetasks/StartBackgroundService.java
+++ app/src/main/java/com/dailyroutinetasks/StartBackgroundService.java
@@ -1,26 +1,34 @@
 package com.dailyroutinetasks;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
+import androidx.core.content.ContextCompat;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class StartBackgroundService extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        if(!BackgroundWorker.isServiceStarted) {
-            BackgroundWorker.isServiceStarted = true;
-            WorkRequest backgroundWorker =
-                    new OneTimeWorkRequest.Builder(BackgroundWorker.class)
-                            .build();
-            WorkManager
-                    .getInstance(context)
-                    .enqueue(backgroundWorker);
+        if(!BackgroundService.isServiceStarted) {
+            BackgroundService.isServiceStarted = true;
+
+            //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                //context.startForegroundService(new Intent(context, BackgroundService.class));
+            //} else {
+                context.startService(new Intent(context, BackgroundService.class));
+            //}
+
         }
     }
 }
